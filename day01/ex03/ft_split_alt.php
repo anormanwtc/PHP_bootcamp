19178c8e00c5debdd006bcfbb58559eaf2d2381b
Alistair Norman <anorman@c5r4s3.wethinkcode.co.za>
2019-10-09 16:03:45 +0200

    removed extra uneeded filtering

--- day01/ex03/ft_split_alt.php
+++ day01/ex03/ft_split_alt.php
@@ -2,9 +2,6 @@
 	function ft_split($input)
 	{
 		$split = preg_split('/\s+/', $input);
-		#separates the string into a 2D by spaces
-		$split = array_filter($split, 'strlen');
-		#removes the array elements where strlen is false
 		sort($split);
 		return $split;
 	}

50a0552e875cffcc96306e5a0ab5aacfd088dcd2
Alistair Norman <anorman@c5r4s3.wethinkcode.co.za>
2019-10-09 14:58:53 +0200

    small function to display all defined functions nicked from the internet

---
+++ day01/defined_functs.php
@@ -0,0 +1,25 @@
+<?php
+$funcs = get_defined_functions();
+readline
+foreach ($funcs['internal'] as $func) {
+    $f = new ReflectionFunction($func);
+    $line = '';
+
+    $line .= $func . ' ( ';
+
+    $params = '';
+    $optional = 0;
+    foreach ($f->getParameters() as $param) {
+        if ($param->isOptional()) {
+            $params .= '[ ';
+            $optional++;
+        }
+
+        $params .= ($param->isPassedByReference() ? '&' : '')
+                   . '$' . $param->getName()
+                   . ', ';
+    }
+
+    $line .= substr($params, 0, -2) . str_repeat(' ]', $optional) . " )\n";
+    echo $line;
+}

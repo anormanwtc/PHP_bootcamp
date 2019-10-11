f35c7513ff53162730adcf8689aac131ed430696
Alistair Norman <anorman@c5r4s3.wethinkcode.co.za>
2019-10-11 10:36:30 +0200

    trim/pregsplit for whitespace

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -3,13 +3,13 @@
 if ($argc < 2)
 	exit;
 unset($argv[0]);
-$final = implode(" ", $argv);
-$final = explode(" ", $final);
+$final = trim(implode(" ", $argv));
+$final = preg_split('/\s+/', $final);
 sort($final);
 $cnt = 0;
-while ($final[$cnt])
+while (($final[$cnt]))
 {
-	echo $final[$cnt] . "\n";
+	echo $final[$cnt] ."\n";
 	$cnt += 1;
 }
 ?>
